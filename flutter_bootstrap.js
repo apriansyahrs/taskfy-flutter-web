882f21db140000d6335e97ff0e67ddf102af6ead
apriansyahrs <apriansyahrizs@gmail.com>
2025-02-15 15:00:27 +0700

    up

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2426019416"
+    serviceWorkerVersion: "1838744128"
   }
 });
